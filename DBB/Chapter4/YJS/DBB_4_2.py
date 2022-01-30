577465da784b253ecedfc6bd6bc91ec197b72ace
DESKTOP-20KSQHM\dbs10 <dbs106@gmail.com>
2022-01-30 19:12:52 +0900

    loop 없는 풀이 (3중 loop 있어도 초당 4만 정도의 연산이라 괜찮긴 함)

--- DBB/Chapter4/YJS/DBB_4_2.py
+++ DBB/Chapter4/YJS/DBB_4_2.py
@@ -1,13 +1,19 @@
 # 시간제한: 2초
 # 메모리제한: 128mb
 N = int(input())
-s = [i for i in range(60) if '3' in str(i)]
-m = [i for i in range(60) if '3' in str(i)]
-h = [i for i in range(N) if '3' in str(i)]
-s_num = len(s)
-m_num = len(m)
-h_num = len(h)
-a = s_num*60*(N+1)
-b = (60-s_num)*m_num*(N+1)
-c = (60-s_num)*(60-m_num)*h_num
-print(a+b+c)
+min_sec = [i for i in range(60) if '3' in str(i)]
+ms_num = len(min_sec)
+h_num = 0 
+if N<3:
+    h_num = 0 
+elif N<13:
+    h_num = 1
+elif N<23:
+    h_num = 2
+else:
+    h_num = 3 
+
+h_case = h_num*60*60
+m_case = (N-h_num+1)*ms_num*60
+s_case = (N-h_num+1)*(60-ms_num)*ms_num
+print(h_case+m_case+s_case)
