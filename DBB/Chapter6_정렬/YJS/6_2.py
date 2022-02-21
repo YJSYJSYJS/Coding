93b53813a1a0c248a8ca27ba767e19d2906b7186
DESKTOP-20KSQHM\dbs10 <dbs106@gmail.com>
2022-02-21 18:33:38 +0900

    내장함수(sorted()) 활용 O(NlogN)

--- DBB/Chapter6_정렬/YJS/6_2.py
+++ DBB/Chapter6_정렬/YJS/6_2.py
@@ -3,23 +3,23 @@ N: 1~100000
 score: 1~100
 name: len <= 100
 성적 내림차순 이름 출력(동일 성적 이름 임의의 순서 가능)
+1sec -> O(NlogN), 128mb
 '''
 N = int(input())
-scores = [i for i in range(100,0,-1)]
-scores_dict = dict.fromkeys(scores, [])
-
+scores_dict = dict()
 for n in range(N):
     c_name, c_score = input().split()
-    scores_dict[int(c_score)].append(c_name)
-print(scores_dict)
+    print(c_score)
+    if int(c_score) not in scores_dict.keys():
+        scores_dict[int(c_score)] = [c_name]
+    else:
+        scores_dict[int(c_score)].append(c_name)
+
+sorted_scores = sorted(scores_dict.items())
 result = list()
 
-for i in range(100,0,-1):
-    curr_names = scores_dict[i]
-    if len(curr_names)==0:
-        continue
-    else:
-        result = result + curr_names
+for s in sorted_scores:
+    result = result + s[1]
 
 for r in result:
     print(r, end=' ')     
